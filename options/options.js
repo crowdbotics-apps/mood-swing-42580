9459b1addd9cb0732406a25879f7b48b8472bae5
Crowdbotics <team@crowdbotics.com>
2023-08-02 01:42:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mood-swing-42580",
+  url: "https://mood-swing-42580.botics.co",
+  api: "https://mood-swing-42580.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
